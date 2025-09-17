a7d60f8732e41b17177538856b652afbf4979955
Thomas Mattern <thomasmattern1@gmail.com>
2025-09-17 13:44:52 -0600

    Create basic websocket for react to consumer

--- sw/Server/server.py
+++ sw/Server/server.py
@@ -1,6 +1,14 @@
 from flask import Flask, jsonify, request, Response
 from flask_cors import CORS
 from flask_socketio import SocketIO, send, emit
+import time
+import threading
+import os
+import sys
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(script_dir, ".."))
+sys.path.insert(0, project_root)
 
 from Scripts.core.vehicle import *
 
@@ -12,7 +20,6 @@ socketio = SocketIO(app)
 # probably move this out of server logic
 vehicle = Vehicle()
 
-
 @app.route('/arm', methods=['POST'])
 def arm_vehicle():
     vehicle.arm_vehicle()
@@ -23,6 +30,18 @@ def disarm_vehicle():
     vehicle.disarm_vehicle()
     return jsonify({'message': 'Vehicle disarmed successfully'}), 200
 
+
+def telemetry_thread():
+    while True:
+        msg_type = "GPS_RAW_INT"
+        data = {"roll": 10, "pitch": 10, "yaw": 10}
+
+        if data:
+            socketio.emit("vehicle_state", {"type": msg_type, "data": data})
+
+        time.sleep(0.1)
+
+threading.Thread(target=telemetry_thread, daemon=True).start()
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=5000)
--- sw/TestScripts/unit_tests/server_test.py
+++ sw/TestScripts/unit_tests/server_test.py
@@ -7,6 +7,9 @@ project_root = os.path.abspath(os.path.join(script_dir, "../.."))
 sys.path.insert(0, project_root)
 
 from Server import server
+from flask_socketio import SocketIO
+from unittest.mock import patch
+
 
 @pytest.fixture()
 def app():
@@ -33,3 +36,20 @@ def test_disarm(client):
     response = client.post("/disarm")
     assert response.status_code == 200
     assert b"Vehicle disarmed successfully" in response.data
+
+# Helper
+def run_telemetry_once():
+    # Dummy Data
+    msg_type = "GPS_RAW_INT"
+    data = {"roll": 10, "pitch": 10, "yaw": 10}
+    if data:
+        # Emit vehicle state
+        server.socketio.emit("vehicle_state", {"type": msg_type, "data": data})
+
+def test_telemetry_emit():
+    with patch.object(server.socketio, 'emit') as emit_mock:
+        run_telemetry_once()
+        emit_mock.assert_called_once_with(
+            "vehicle_state",
+            {"type": "GPS_RAW_INT", "data": {"roll": 10, "pitch": 10, "yaw": 10}}
+        )
